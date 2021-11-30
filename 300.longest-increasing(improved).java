2e6f947c9b26c6f8f96e844fb0cdbd028978f322
tianhong0v0 <tianhongsayhelloworld@gmail.com>
2021-11-30 12:04:44 +0800

    problem 300, improved solution

---
+++ 300.longest-increasing(improved).java
@@ -0,0 +1,26 @@
+import java.util.Arrays;
+
+/*
+ * @lc app=leetcode id=300 lang=java
+ *
+ * [300] Longest Increasing Subsequence
+ */
+
+// @lc code=start
+class Solution {
+    public int lengthOfLIS(int[] nums) {
+        if (nums.length == 1) return 1;
+        int[] dp = new int[nums.length];
+        int res = 0;
+        Arrays.fill(dp, 1);
+        for(int current=0; current<nums.length; current++){
+            for(int i=0; i<current; i++){
+                dp[current] = (nums[current]>nums[i]) ? Math.max(dp[current], dp[i]+1):dp[current];
+            }
+            res = (dp[current]>res)?dp[current]:res;
+        }
+        return res;
+    }
+}
+// @lc code=end
+

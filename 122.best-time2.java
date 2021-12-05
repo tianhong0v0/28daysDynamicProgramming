04dc5c9a52667275ce81867838dc1babbd233a21
tianhong0v0 <tianhongsayhelloworld@gmail.com>
2021-12-05 19:05:57 +0800

    day 7, upgrade this and solve stock problem III

---
+++ 122.best-time2.java
@@ -0,0 +1,16 @@
+class Solution {
+	public static int maxProfit(int[] prices) {
+		int[][] dp = new int[prices.length][2];
+		dp[0][0] = 0 - prices[0];
+		dp[0][1] = 0;
+		for(int i=1; i<prices.length; i++){
+			dp[i][0] = Math.max(dp[i-1][0], dp[i-1][1]-prices[i]);
+			dp[i][1] = Math.max(dp[i-1][0]+prices[i], dp[i-1][1]);
+		}
+		return dp[prices.length-1][1];
+	}
+	public static void main(String[] args) {
+		int[] prices = new int[]{1,2,3,4,5};
+		System.out.println(maxProfit(prices));
+	}
+}

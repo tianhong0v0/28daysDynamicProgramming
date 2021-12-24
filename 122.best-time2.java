4e26b8de6162374357b3074a221341f449d12c50
tianhong0v0 <tianhongsayhelloworld@gmail.com>
2021-12-24 16:38:08 +0800

    adding more comments

--- 122.best-time2.java
+++ 122.best-time2.java
@@ -1,6 +1,8 @@
 class Solution {
 	public static int maxProfit(int[] prices) {
 		int[][] dp = new int[prices.length][2];
+		//dp[i][0] means the maximum amount of profits earned with stock on hands
+		//dp[i][1] means the maximum amount of profits earned with cash (without stock) on hands
 		dp[0][0] = 0 - prices[0];
 		dp[0][1] = 0;
 		for(int i=1; i<prices.length; i++){
